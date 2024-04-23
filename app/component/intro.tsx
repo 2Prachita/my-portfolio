facbaba63be0144b72b0d447e477bca783ab849e
2Prachita <prachitakotangale@gmail.com>
2024-04-23 19:46:40 +0530

    Updated the Linkedin URL

--- app/component/intro.tsx
+++ app/component/intro.tsx
@@ -90,7 +90,7 @@ export default function Intro() {
             <a 
             className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15]
             active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
-            href='https://www.linkedin.com/in/prachita-kotangale-279b96153/' target='_blank'>
+            href='https://www.linkedin.com/in/prachita-kotangale/' target='_blank'>
                 <BsLinkedin/>
             </a>
             <a 
